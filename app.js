474399c79d1e8f5d92a28c23e2d6aae75d603dd9
ty18881 <ty18881@gmail.com>
2020-05-31 14:28:22 -0400

    added saving and updating list in local storage

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+/**
+ * Following Dev Ed tutorial - Beginner Vanilla Javascript Project Tutorial
+ * https://www.youtube.com/watch?v=Ttf3CEsEwMQ&feature=emb_rel_end
+ * 
+ * FUTURE ENHANCEMENT - display completed items from local storage correctly.
+ */
+
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
 
 /**Event Listeners */
-
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 
 todoList.addEventListener('click', deleteCheck);
@@ -14,7 +21,6 @@ filterOption.addEventListener('click', filterTodos);
 function addTodo(event) {
 
     event.preventDefault();
-    // console.log('Hello From Add function');
 
     // create a to-do div 
 
@@ -35,6 +41,7 @@ function addTodo(event) {
 // append the newTodo as a child to the todoDiv
 
 todoDiv.appendChild(newTodo);
+
 
 // adding status update buttons
 
@@ -59,6 +66,11 @@ todoDiv.appendChild(removeButton);
 
 todoList.appendChild(todoDiv);
 
+
+// save new item to localStorage
+
+saveLocalTodos(todoInput.value);
+
 // clear the form input
 
 todoInput.value = "";
@@ -74,6 +86,7 @@ function deleteCheck(event){
     
     if (item.classList[0] === 'remove-button'){
         const todo = item.parentElement;
+        removeTodoFromLocalStorage(todo);
         todo.remove();
     }
 
@@ -127,4 +140,124 @@ function filterTodos(event) {
 
 
 }
+
+
+    /** Save the todo list to local storage 
+     * if we already have a to-do list stored,
+     * just add the new item instead of rewriting the list.
+    */
+
+function saveLocalTodos (todo) {
+
+    let todos;
+
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        // get the existing list from localStorage.
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.push(todo);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+/**Retrieve to-do list held in local storage when we 
+ * restart our application
+ */
+
+ function getTodos () {
+     let todos;
+
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    showSavedTodoList(todos);
+    
+ }
+
+/**
+ * 
+ * Display the to-do list from local storage on the screen. 
+ */
+ function showSavedTodoList (todos) {
+     todos.forEach( function(todo){
+
+
+        const todoDiv = document.createElement('div');
+
+                todoDiv.classList.add('todo');
+
+                // create LI inside of the list
+
+                const newTodo = document.createElement('li');
+
+                // capture the value the user has typed into the input box.
+                
+                newTodo.innerText= todo;
+
+                newTodo.classList.add('todo-item');
+
+            // append the newTodo as a child to the todoDiv
+
+            todoDiv.appendChild(newTodo);
+
+
+            // adding status update buttons
+
+            const completedButton = document.createElement('button');
+
+            completedButton.innerHTML= '<i class="fas fa-check"> </i>';
+
+            completedButton.classList.add('complete-button');
+
+            todoDiv.appendChild(completedButton);
+
+            const removeButton = document.createElement('button');
+
+            removeButton.innerHTML= '<i class="fas fa-trash"> </i>';
+
+            removeButton.classList.add('remove-button');
+
+            todoDiv.appendChild(removeButton);
+
+
+            // add this to the list
+
+            todoList.appendChild(todoDiv);
+     });
+ }
+
+ /**Remove an item from the todo list in local storage */
+
+ function removeTodoFromLocalStorage(todo) {
+
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    console.log(todo);
+
+    // need to retrieve the index of the element that is being deleted.
+    // then save the revised to-do list to local storage.
+    // NOTE:  todo is the DIV of the item we are removing.
+
+    const todoIndex = todo.children[0].innerText;
+
+    console.log('Remove Item at index ', todos.indexOf(todoIndex));
+
+    todos.splice(todos.indexOf(todoIndex), 1);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+
+ }
 
